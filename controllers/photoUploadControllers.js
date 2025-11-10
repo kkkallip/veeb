39a0f477d7a8b4b8264a518fa12294621340608a
Karl K. Kallip <kkkallip@users.noreply.github.com>
2025-11-10 11:16:41 +0200

    Add watermarking

--- controllers/photoUploadControllers.js
+++ controllers/photoUploadControllers.js
@@ -2,6 +2,7 @@ const dbinfo = require("../../../vp2025config");
 const mysql = require("mysql2/promise");
 const fs = require("fs").promises;
 const sharp = require("sharp");
+const watermarkFile = "./public/images/vp_logo_small.png";
 
 const dbConf = {
     host: dbinfo.configData.host,
@@ -26,8 +27,24 @@ const photoUploadPost = async (req, res)=>{
         const fileName = "vp_" + Date.now() + ".jpg"
         console.log(fileName);
         await fs.rename(req.file.path, req.file.destination + fileName);
+
+        const watermarkSettings = [{
+            input: watermarkFile,
+            gravity: "southeast"
+        }];
+		if (!await fs.access(watermarkFile).then(() => true).catch(() => false)) {
+             console.log("Vesimأ¤rgi faili ei leitud!");
+             // Tأ¼hjendame seaded, et vesimأ¤rki ei proovitaks lisada
+             watermarkSettings.length = 0; 
+        }
+        let normalImageProcessor = await sharp(req.file.destination + fileName).resize(800, 600).jpeg({quality: 90});
+        if (watermarkSettings.length > 0) {
+            normalImageProcessor = await normalImageProcessor.composite(watermarkSettings);
+        }
+		await normalImageProcessor.toFile("./public/gallery/normal/" + fileName);
+
         //loon normaalsuuruse 800x600
-        await sharp(req.file.destination + fileName).resize(800, 600).jpeg({quality: 90}).toFile("./public/gallery/normal/" + fileName);
+        //await sharp(req.file.destination + fileName).resize(800, 600).jpeg({quality: 90}).toFile("./public/gallery/normal/" + fileName);
         //loon thumbnail pildi 100x100
         await sharp(req.file.destination + fileName).resize(100, 100).jpeg({quality: 90}).toFile("./public/gallery/thumbs/" + fileName);
         conn = await mysql.createConnection(dbConf);
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const dateEt = require("./src/dateTimeET");
 const fs = require("fs");
 const textRef = "public/txt/vanasonad.txt";
 const bodyParser = require("body-parser");
+const dbinfo = require("../../vp2025config");
+const mysql = require("mysql2/promise");
 
 const app = express();
 app.set("view engine", "ejs");
@@ -10,8 +12,33 @@ app.use(express.static("public"));
 //Kui vormist ainult tekst -> false, muidu true
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", (req, res)=> {
-    res.render("index")
+const dbConf = {
+    host: dbinfo.configData.host,
+    user: dbinfo.configData.user,
+    password: dbinfo.configData.passWord,
+    database: dbinfo.configData.dataBase
+}
+
+app.get("/", async (req, res)=> {
+    let conn;
+    try {
+        conn = await mysql.createConnection(dbConf);
+        let sqlReq = "SELECT filename, alttext FROM galleryphotos_ta WHERE id=(SELECT MAX(id) FROM galleryphotos_ta WHERE privacy=? AND deleted IS NULL)";
+		const privacy = 3;
+		const [rows, fields] = await conn.execute(sqlReq, [privacy]);
+		let imgAlt = "Avalik foto";
+		if(rows[0].alttext != ""){
+			imgAlt = rows[0].alttext;
+		}
+		res.render("index", {imgFile: "gallery/normal/" + rows[0].filename, imgAlt: imgAlt});
+    } catch (error) {
+        console.log(error);
+        res.render("index", {imgFile: "", imgAlt: "Pilt puudub"});
+    } finally {
+        if(conn) {
+			await conn.end();
+		}
+    }
 });
 
 app.get("/timenow", (req, res)=> {
